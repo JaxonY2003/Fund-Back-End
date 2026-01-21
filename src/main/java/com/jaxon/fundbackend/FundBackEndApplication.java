a3d61b07a50bcd77edad3fddabfa20d2f00fec63
JaxonY2003 <xupingy@nuaa.edu.cn>
2026-01-21 19:51:07 +0800

    Initial commit

---
+++ src/main/java/com/jaxon/fundbackend/FundBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.jaxon.fundbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jaxon/fundbackend/FundBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaxon.fundbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
